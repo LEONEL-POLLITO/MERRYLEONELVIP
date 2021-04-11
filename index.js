7610163f0c08703c2bb1696407a902de2510a4e6
LEONEL-POLLITO <81774877+LEONEL-POLLITO@users.noreply.github.com>
2021-04-11 15:07:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const {
     CeerTod
 } = ngonsol
 // POWERED BY MrG{108P}*
-prefix = "#"
+prefix = "!"
 blocked = []   
 limitawal = 1200
 memberlimit = 2
@@ -766,369 +766,51 @@ me = MrG.user
 uptime = process.uptime()
 buffer = await getBuffer(me.imgUrl)
 				    const uangku = checkATMuser(sender)
-shiz = `┏━━━━《 *${botName}*  シ︎ 》━━━━
-┃
-┃ ❏ *NAMA : ${pushname}*
-┃ ❏ *Prefix*: 「 ${prefix} 」
-┃ ❏ *UANG : Rp:${uangku}*
-┃ ❏ *Totar: ${mek.length}*
-┃
-┣ ❏ *ABOUT BOT* 」
-┃
-┣ ❏ *${prefix}info*
-┣ ❏ *${prefix}ping*
-┣ ❏ *${prefix}donasi*
-┣ ❏ *${prefix}owner* 
-┃
-┣ ❏ *ANIME MENU* 」
-┃
-┣ ❏ *${prefix}naruto*
-┣ ❏ *${prefix}boruto*
-┣ ❏ *${prefix}minato*
-┣ ❏ *${prefix}hinata*
-┣ ❏ *${prefix}sakura*
-┣ ❏ *${prefix}sasuke*
-┣ ❏ *${prefix}kaneki*
-┣ ❏ *${prefix}toukachan*
-┣ ❏ *${prefix}rize*
-┣ ❏ *${prefix}akira*
-┣ ❏ *${prefix}itori*
-┣ ❏ *${prefix}kurumi*
-┣ ❏ *${prefix}miku*
-┣ ❏ *${prefix}rem*
-┣ ❏ *${prefix}animecry*
-┣ ❏ *${prefix}neonime*
-┣ ❏ *${prefix}animekiss*
-┣ ❏ *${prefix}wink*
-┣ ❏ *${prefix}waifu*
-┣ ❏ *${prefix}husbu*
-┣ ❏ *${prefix}loli*
-┣ ❏ *${prefix}waifukawai*
-┣ ❏ *${prefix}kemonomimi*
-┣ ❏ *${prefix}kuni*
-┣ ❏ *${prefix}nsfwloli3*
-┣ ❏ *${prefix}neko*
-┣ ❏ *${prefix}holo*
-┣ ❏ *${prefix}kusonime*
-┣ ❏ *${prefix}pictwaifu*
-┣ ❏ *${prefix}pictlolicon*
-┣ ❏ *${prefix}pictneko*
-┣ ❏ *${prefix}miku*
-┣ ❏ *${prefix}rikka*
-┣ ❏ *${prefix}animesad*
-┣ ❏ *${prefix}yotsuba*
-┣ ❏ *${prefix}sagiri*
-┣ ❏ *${prefix}ohto*
-┣ ❏ *${prefix}randomanime*
-┃
-┣ ❏ *DOMPET* 」
-┃
-┣ ❏ *${prefix}limit*
-┣ ❏ *${prefix}atm*
-┣ ❏ *${prefix}transfer*
-┣ ❏ *${prefix}buylimit*
-┃
-┣ ❏ *DOWNLOADER* 」
-┃
-┣ ❏ *${prefix}igvideo*
-┣ ❏ *${prefix}soundcloud*
-┣ ❏ *${prefix}tiktok*
-┣ ❏ *${prefix}tiktoknowm*
-┣ ❏ *${prefix}ytmp4*
-┣ ❏ *${prefix}ytmp3*
-┣ ❏ *${prefix}play*
-┣ ❏ *${prefix}play2*
-┃
-┣ ❏ *GROUP MENU* 」
-┃
-┣ ❏ *${prefix}welcome* 「1/0」
-┣ ❏ *${prefix}leveling* 「1/0」
-┣ ❏ *${prefix}simih* 「1/0」
-┣ ❏ *${prefix}simi
-┣ ❏ *${prefix}nsfw* 「1/0」
-┣ ❏ *${prefix}antilinkgrup* 「1/0」
-┣ ❏ *${prefix}grup* 「buka/tutup」
-┣ ❏ *${prefix}add*
-┣ ❏ *${prefix}kick*
-┣ ❏ *${prefix}hedsot*
-┣ ❏ *${prefix}linkgrup*
-┣ ❏ *${prefix}promote*
-┣ ❏ *${prefix}demote*
-┣ ❏ *${prefix}setname*
-┣ ❏ *${prefix}setdesc*
-┣ ❏ *${prefix}leave*
-┣ ❏ *${prefix}tagall*
-┣ ❏ *${prefix}admin*
-┣ ❏ *${prefix}level*
-┣ ❏ *${prefix}fitnah*
-┣ ❏ *${prefix}hidetag*
-┃
-┣ ❏ *MAKER MENU* 」
-┃
-┣ ❏ *${prefix}hartatahta*
-┣ ❏ *${prefix}glowneon*
-┣ ❏ *${prefix}gsuggest*
-┣ ❏ *${prefix}candlemug*
-┣ ❏ *${prefix}lovemss*
-┣ ❏ *${prefix}mugflower*
-┣ ❏ *${prefix}narutobanner*
-┣ ❏ *${prefix}battelfiel*
-┣ ❏ *${prefix}8bit*
-┣ ❏ *${prefix}blueneon*
-┣ ❏ *${prefix}matrix*
-┣ ❏ *${prefix}breakwall*
-┣ ❏ *${prefix}greenneon*
-┣ ❏ *${prefix}dropwater*
-┣ ❏ *${prefix}wolflogo*
-┣ ❏ *${prefix}flowertext*
-┣ ❏ *${prefix}crosslogo*
-┣ ❏ *${prefix}silktext*
-┣ ❏ *${prefix}flametext*
-┣ ❏ *${prefix}glowtext*
-┣ ❏ *${prefix}smoketext*
-┣ ❏ *${prefix}pubglogo*
-┣ ❏ *${prefix}skytext*
-┣ ❏ *${prefix}hpotter*
-┣ ❏ *${prefix}lighttext*
-┣ ❏ *${prefix}retrotext*
-┣ ❏ *${prefix}crismes*
-┣ ❏ *${prefix}snowwrite*
-┣ ❏ *${prefix}watercolor*
-┣ ❏ *${prefix}firework*
-┣ ❏ *${prefix}sandwrite*
-┣ ❏ *${prefix}epep*
-┣ ❏ *${prefix}gplaybutton*
-┣ ❏ *${prefix}splaybutton*
-┣ ❏ *${prefix}text3dbox*
-┣ ❏ *${prefix}text3d*
-┣ ❏ *${prefix}phlogo*
-┣ ❏ *${prefix}bplogo*
-┣ ❏ *${prefix}leavestext*
-┣ ❏ *${prefix}textlight*
-┣ ❏ *${prefix}glitchtext*
-┣ ❏ *${prefix}crossfirelogo*
-┣ ❏ *${prefix}woodblock*
-┣ ❏ *${prefix}candlemug*
-┣ ❏ *${prefix}coffeecup*
-┣ ❏ *${prefix}coffeecup2*
-┣ ❏ *${prefix}hackeravatar*
-┣ ❏ *${prefix}jokerlogo*
-┣ ❏ *${prefix}halloween*
-┃
-┣◪ *SERTI MENU* 」
-┃
-┣ ❏ *${prefix}babuserti*
-┣ ❏ *${prefix}bucinserti*
-┣ ❏ *${prefix}bocilepepserti*
-┣ ❏ *${prefix}gayserti*
-┣ ❏ *${prefix}pacarserti*
-┣ ❏ *${prefix}sadboyserti*
-┣ ❏ *${prefix}surgaserti*
-┣ ❏ *${prefix}pintarserti*
-┣ ❏ *${prefix}badboyserti*
-┣ ❏ *${prefix}badgirlserti*
-┣ ❏ *${prefix}goodboyserti*
-┣ ❏ *${prefix}goodgirlserti*
-┣ ❏ *${prefix}editorberkelasserti*
-┣ ❏ *${prefix}goodlookingserti*
-┣ ❏ *${prefix}fucekboyserti*
-┣ ❏ *${prefix}jametserti*
-┣ ❏ *${prefix}hekelserti*
-┣ ❏ *${prefix}fftourserti*
-┣ ❏ *${prefix}fftourserti2*
-┣ ❏ *${prefix}fftourserti3*
-┣ ❏ *${prefix}fftourserti4*
-┣ ❏ *${prefix}fftourserti5*
-┣ ❏ *${prefix}pubgtourserti*
-┣ ❏ *${prefix}pubgtourserti2*
-┣ ❏ *${prefix}pubgtourserti3*
-┣ ❏ *${prefix}pubgtourserti4*
-┣ ❏ *${prefix}pubgtourserti5*
-┣ ❏ *${prefix}mltourserti*
-┣ ❏ *${prefix}mltourserti2*
-┣ ❏ *${prefix}mltourserti3*
-┣ ❏ *${prefix}mltourserti4*
-┣ ❏ *${prefix}mltourserti5*
-┣ ❏ *${prefix}tweetfake*
-┃
-┣ ❏ *OTHER MENU* 」
-┃
-┣ ❏ *${prefix}happymod*
-┣ ❏ *${prefix}pantun*
-┣ ❏ *${prefix}quotes*
-┣ ❏ *${prefix}tribunnews*
-┣ ❏ *${prefix}nickepep*
-┣ ❏ *${prefix}liputan*
-┣ ❏ *${prefix}foxnews*
-┣ ❏ *${prefix}playstore*
-┣ ❏ *${prefix}searchsticker*
-┣ ❏ *${prefix}igstalk*
-┣ ❏ *${prefix}shopee*
-┣ ❏ *${prefix}ytsearch*
-┣ ❏ *${prefix}kbbi*
-┣ ❏ *${prefix}apkpure*
-┣ ❏ *${prefix}igsearch*
-┣ ❏ *${prefix}wiki*
-┣ ❏ *${prefix}film*
-┣ ❏ *${prefix}film2*
-┣ ❏ *${prefix}komik*
-┣ ❏ *${prefix}spamcall*
-┣ ❏ *${prefix}spamgmail*
-┣ ❏ *${prefix}tebakgambar*
-┣ ❏ *${prefix}tebakbendera*
-┣ ❏ *${prefix}caklontong*
-┣ ❏ *${prefix}sidshort*
-┣ ❏ *${prefix}asupan*
-┃
-┣ ❏ *DEFACER MENU* 」
-┃
-┣ ❏ *${prefix}dorking*
-┣ ❏ *${prefix}encode64*
-┣ ❏ *${prefix}decode64*
-┣ ❏ *${prefix}decode32*
-┣ ❏ *${prefix}encode32*
-┣ ❏ *${prefix}encbinary*
-┣ ❏ *${prefix}decbinary*
-┣ ❏ *${prefix}encoctal*
-┣ ❏ *${prefix}decoctal*
-┣ ❏ *${prefix}becrypt*
-┣ ❏ *${prefix}hashidentifier*
-┃
-┣ ❏ *ISLAM MENU* 」
-┃
-┣ ❏ *${prefix}asmaulhusna*
-┣ ❏ *${prefix}niatsholat*
-┣ ❏ *${prefix}doaharian*
-┣ ❏ *${prefix}bacaansholat*
-┣ ❏ *${prefix}kisahnabi*
-┣ ❏ *${prefix}tahlil*
-┣ ❏ *${prefix}ayatkursi*
-┣ ❏ *${prefix}quotemuslim*
-┣ ❏ *${prefix}randomquran*
-┣ ❏ *${prefix}quranaudio*
-┃
-┣ ❏ *OWNER MENU* 」
-┃
-┣ ❏ *${prefix}bc*
-┣ ❏ *${prefix}bcgc*
-┣ ❏ *${prefix}clearall*
-┣ ❏ *${prefix}block*
-┣ ❏ *${prefix}unblock*
-┣ ❏ *${prefix}clone*
-┣ ❏ *${prefix}setppbot*
-┣ ❏ *${prefix}setreplay*
-┣ ❏ *${prefix}setprefix*
-┣ ❏ *${prefix}addprem*
-┣ ❏ *${prefix}dellprem*
-┣ ❏ *${prefix}ban*
-┣ ❏ *${prefix}unban*
-┣ ❏ *${prefix}event1/0*
-┃
-┣ ❏ *PREMIUM MENU* 」
-┃
-┣ ❏ *${prefix}premiumlist*
-┣ ❏ *${prefix}randomhentai*
-┣ ❏ *${prefix}bokep*
-┣ ❏ *${prefix}blowjob*
-┣ ❏ *${prefix}nulis1*
-┣ ❏ *${prefix}nulis2*
-┣ ❏ *${prefix}mutual*
-┣ ❏ *${prefix}next*
-┣ ❏ *${prefix}nangis*
-┣ ❏ *${prefix}cium*
-┣ ❏ *${prefix}peluk*
-┣ ❏ *${prefix}tiktok*
-┣ ❏ *${prefix}yutubdl*
-┣ ❏ *${prefix}hidetag5*
-┣ ❏ *${prefix}hidetag10*
-┣ ❏ *${prefix}moddroid*
-┣ ❏ *${prefix}happymod*
-┣ ❏ *${prefix}ytmp3*
-┣ ❏ *${prefix}ytmp4*
-┣ ❏ *${prefix}igvideo*
-┣ ❏ *${prefix}play*
-┣ ❏ *${prefix}play2*
-┃
-┣ ❏ *RANDOM MENU* 」
-┃
-┣ ❏ *${prefix}nekonime*
-┣ ❏ *${prefix}loli*
-┣ ❏ *${prefix}neko2*
-┣ ❏ *${prefix}husbu*
-┣ ❏ *${prefix}kpop*
-┣ ❏ *${prefix}anjing*
-┣ ❏ *${prefix}pokemon*
-┣ ❏ *${prefix}bokep*
-┣ ❏ *${prefix}darkjones*
-┃
-┣ ❏ *SIMPLE MENU* 」
-┃
-┣ ❏ *${prefix}sticker*
-┣ ❏ *${prefix}stickerwa*
-┣ ❏ *${prefix}linesticker*
-┣ ❏ *${prefix}ttp*
-┣ ❏ *${prefix}ttp2*
-┣ ❏ *${prefix}ttp3*
-┣ ❏ *${prefix}ttp4*
-┣ ❏ *${prefix}attp*
-┣ ❏ *${prefix}igstory*
-┣ ❏ *${prefix}mediafire*
-┣ ❏ *${prefix}tts*
-┣ ❏ *${prefix}toimg*
-┣ ❏ *${prefix}nulis*
-┣ ❏ *${prefix}stalking*
-┣ ❏ *${prefix}quotes*
-┣ ❏ *${prefix}bikinquote*
-┣ ❏ *${prefix}darkjokes*
-┣ ❏ *${prefix}memeindo*
-┣ ❏ *${prefix}profile*
-┃
-┣ ❏ *SOUND MENU* 」
-┃
-┣ ❏ *${prefix}iri*
-┣ ❏ *${prefix}pale*
-┣ ❏ *${prefix}sound1*
-┣ ❏ *${prefix}sound2*
-┣ ❏ *${prefix}sound3*
-┣ ❏ *${prefix}sound4*
-┣ ❏ *${prefix}sound5*
-┣ ❏ *${prefix}sound6*
-┣ ❏ *${prefix}sound7*
-┣ ❏ *${prefix}sound8*
-┣ ❏ *${prefix}sound9*
-┣ ❏ *${prefix}sound10*
-┣ ❏ *${prefix}sound11*
-┣ ❏ *${prefix}sound12*
-┣ ❏ *${prefix}sound13*
-┣ ❏ *${prefix}sound14*
-┣ ❏ *${prefix}sound15*
-┣ ❏ *${prefix}sound16*
-┣ ❏ *${prefix}sound17*
-┣ ❏ *${prefix}sound18*
-┣ ❏ *${prefix}sound19*
-┣ ❏ *${prefix}bernyanyi*
-┣ ❏ *${prefix}kebenaran*
-┣ ❏ *${prefix}sad*
-┣ ❏ *${prefix}sad2*
-┣ ❏ *${prefix}sad3*
-┣ ❏ *${prefix}candagodloking*
-┣ ❏ *${prefix}babycute*
-┣ ❏ *${prefix}cidro2*
-┣ ❏ *${prefix}gam2piri*
-┣ ❏ *${prefix}akusukakamu*
-┣ ❏ *${prefix}terpesona*
-┣ ◪ *Thanks To*
-┃
-┣ ❏ *Mr.108P*
-┣ ❏ *SofyanAmv*
-┣ ❏ *Ivan_maulana*
-┣ ❏ *Arashi~*
-┣ ❏ *Mr.A43G*
-┣ ❏ *ItsmeikyXSec404*
-┣ ❏ *Fadhill*
-┣ ❏ *Ramlan*
-┃
-┗━━━━《 Rem Bot -X シ︎ 》━━━━`
+shiz = `*╭•────•(USUARIO)•────•*
+*╰•─➤❝ _[ৣ››🐰͢⸸ғᴋ᭭-ʟᷤᴇͤᴏͧɴͪᴇͤʟᷤ⸸]_ ❞*
+
+╭─────────────❂
+│╭─❉ _*MENUS*_
+├┼➻ *!fullmenu*
+├┼➻ *!menusticker*
+├┼➻ *!menuanime*
+├┼➻ *!nsfwmenu*
+├┼➻ *!imgmenu*
+├┼➻ *!herramientas*
+│╰────────❉
+│
+│╭─❉ _*JUEGOS*_
+││╭➻ *!ppt*
+├┼┼➻ *!piedra*
+├┼┼➻ *!papel*
+││╰➻ *!tijera*
+│├➻ *!marco*
+│├➻ *!polo*
+│├➻ *!repite*
+│├➻ *!death*
+│├➻ *!alguien*
+│├➻ *!beso*
+│├➻ *!cachetada*
+├┼➻ *!gay*
+│╰────────❉
+│
+│╭─❉ _*INFORMACION*_
+├┼➻ *!yo*
+│├➻ *!icono*
+│├➻ *!reglas*
+│├➻ *!creador*
+│├➻ *!espiar*
+├┼➻ *!wa.me*
+│╰────────❉
+│
+│╭─❉ _*ACERCA DE*_
+├┼➻ *!tyc*
+│├➻ *!contratar*
+│├➻ *!desarrollador*
+├┼➻ *!estadistias*
+│╰────────❉
+╰─────────────❂
+`
 MrG.sendMessage(from, buffer, image, { quoted: mek, caption: shiz })
 const menu2 = fs.readFileSync('./assest/menu2.mp3');
 MrG.sendMessage(from, menu2, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
